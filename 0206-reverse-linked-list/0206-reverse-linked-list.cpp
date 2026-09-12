85adf3b5da4f3fcf6bb45ff41adbb02682b15dd9
RANJAN KUMAR CHAUDHARY <chauhanranjan8978@gmail.com>
2026-09-12 20:50:27 +0530

    Time: 0 ms (100%), Space: 13.3 MB (92.99%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -15,10 +15,10 @@ public:
         ListNode* curr = head;
 
         while (curr != nullptr) {
-            ListNode* next = curr->next; // save next node
-            curr->next = prev;           // reverse link
-            prev = curr;                 // move prev
-            curr = next;                 // move curr
+            ListNode* next = curr->next; 
+            curr->next = prev;           
+            prev = curr;                
+            curr = next;                
         }
 
         return prev;
